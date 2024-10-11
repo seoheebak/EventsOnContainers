8d6f5909d4bc5b9c26d6a92f634f8dcef5e6a1ac
SunnyBhatia786 <ranjanasunny786@gmail.com>
2024-10-10 19:04:56 -0700

    Created 2 models

---
+++ EventCatalogAPI/Domain/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace EventCatalogAPI.Domain
+{
+    public class Event
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Location{ get; set; }
+        public string Time { get; set; }
+        public string Duration { get; set; }
+        public decimal Ticket { get; set; }
+        public string PictureUrl { get; set; }
+        public int EventTypeId { get; set; }
+
+        public virtual EventType EventType { get; set; }
+         
+    }
+}
---
+++ EventCatalogAPI/Domain/EventType.cs
@@ -0,0 +1,8 @@
+﻿namespace EventCatalogAPI.Domain
+{
+    public class EventType
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+}
