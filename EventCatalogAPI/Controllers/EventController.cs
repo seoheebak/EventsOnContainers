3b0ca52e146dcc25862f5bfe4a74ca8b4ce86322
seoheebak <seoheebak95@gmail.com>
2024-10-22 22:12:36 -0700

    keep working on the EventController

--- EventCatalogAPI/Controllers/EventController.cs
+++ EventCatalogAPI/Controllers/EventController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventCatalogAPI.Controllers
 {
@@ -7,15 +9,32 @@ namespace EventCatalogAPI.Controllers
     [ApiController]
     public class EventController : ControllerBase
     {
-        [HttpGet("[action]")]
-        public IActionResult EventTypes()
+        private readonly EventContext _context;
+        public EventController (EventContext context)
         { 
-        
+            _context = context;
         }
 
         [HttpGet("[action]")]
-        public IActionResult Items()
+        public async  Task<IActionResult> EventTypes()
         {
+            var types = await _context.EventTypes.ToListAsync();
+            return Ok(types);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery]int pageSize = 6
+            )
+        {
+            var items = await _context.Events
+                .OrderBy(c => c.Id)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            
+            return Ok(items);
         }
     }
 }
